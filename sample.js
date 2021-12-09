a7d60f8a2e5887367985d9e5454802fd29e66180
kalyan-continualengine <58549880+kalyan-continualengine@users.noreply.github.com>
2021-12-09 19:24:06 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
 
-<h2>JavaScript in Body</h2>
-
-<p id="sample"></p>
-
-<script>
-document.getElementById("sample").innerHTML = "Sample File to host";
-</script>
-
-</body>
-</html> 
