05e2e87273e77dbd4b9f874f2544f2ed3f00a0d4
kalyan-continualengine <58549880+kalyan-continualengine@users.noreply.github.com>
2021-12-09 16:00:33 +0530

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript in Body</h2>
+
+<p id="sample"></p>
+
+<script>
+document.getElementById("sample").innerHTML = "Sample File to host";
+</script>
+
+</body>
+</html> 
